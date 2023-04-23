5c3917aaa4fe44f3449772e6869b6c188092253f
Justin Massey <justin.massey@auctane.com>
2023-04-23 16:59:35 -0700

    display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,8 +15,8 @@ const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding={10}
+        padding="px"
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
